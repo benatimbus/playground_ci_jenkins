70ec1406277a90d970092f0ce8535e3e3b4592b1
User <User@DESKTOP-93I3BEB>
2018-11-02 19:28:57 +0100

    first block

--- src/ci/stage/Build.groovy
+++ src/ci/stage/Build.groovy
@@ -1,16 +1,19 @@
 package ci.stage
 
 def call(displayTestResults, failBuildOnMissingResults, displayCoverage, updateDependencies){
-    return stage ("Build") {
-        command = "gradlew clean build" + (updateDependencies ? "--update-dependencies" : "")
-        bat command
+    script{
+        stage ("Build") {
+            command = "gradlew clean build" + (updateDependencies ? "--update-dependencies" : "")
+            bat command
 
-        if(displayTestResults){
-            junit testResults: "/build/test-results/**/*.xml", allowEmptyResults: !failBuildOnMissingResults
-        }
+            if(displayTestResults){
+                junit testResults: "/build/test-results/**/*.xml", allowEmptyResults: !failBuildOnMissingResults
+            }
 
-        if(displayCoverage){
-            step( [ $class: 'JacocoPublisher' ] )
+            if(displayCoverage){
+                step( [ $class: 'JacocoPublisher' ] )
+            }
         }
     }
 }
+
